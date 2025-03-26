ac29b22be8de57f36ece7ca09f30e1d1ef91c2ff
hakuna-tata <610578197@qq.com>
2025-03-27 00:31:07 +0800

    feat: external资源

--- src/generate.ts
+++ src/generate.ts
@@ -1,3 +1,17 @@
-export const generate = async () => {
-    
+import { Schema } from './type';
+import {
+  getMaterialsExternal, 
+  getUtilsExternal,
+} from './globalExternalAssets';
+
+export const generate = async (schema: Schema, options = {}) => {
+  const {
+    materialDeps,
+    usedMaterials,
+    utilDeps,
+    usedUtils,
+  } = schema || {};
+  // 物料库 和 工具库 externals 配置
+  const materialExternalConfig = getMaterialsExternal(materialDeps, usedMaterials);
+  const utilExternalConfig = getUtilsExternal(utilDeps, usedUtils);
 }
---
+++ src/globalExternalAssets.ts
@@ -0,0 +1,15 @@
+import { Schema } from './type';
+
+export const getMaterialsExternal = (
+  deps: Schema['materialDeps'],
+  usedDeps: Schema['usedMaterials']
+) => {
+
+};
+
+export const getUtilsExternal = (
+  deps: Schema['utilDeps'],
+  usedDeps: Schema['usedUtils']
+) => {
+
+};
